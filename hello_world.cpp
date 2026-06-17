31b62a08ae5019686c170e26fe2021811a570d8d
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-18 00:12:47 +0600

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+// we can use here (using namespace std) so we don't need to write std:: every time
 int main(){
     std::cout << "Hello World" << std::endl;
     std::cout << "This is my first C++ program" << std::endl;
