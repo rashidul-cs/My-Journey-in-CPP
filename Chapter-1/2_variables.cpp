82ff57e8ebd69e9c2660694a82a3de7d3630ba1a
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-01 01:35:24 +0600

    Create 2_variables.cpp

---
+++ Chapter-1/2_variables.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x;   //Declearation
+    x = 5;   //Initialization
+    int y;   // we can do also (int y = 9);
+    y = 9;
+    cout << x+y;
+    return 0;
+}
