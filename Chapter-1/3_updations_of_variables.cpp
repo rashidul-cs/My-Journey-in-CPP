eb6de56e0bc387cde838dd540d9f61fea00136e6
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-01 01:39:06 +0600

    Create 3_updations_of_variables.cpp

---
+++ Chapter-1/3_updations_of_variables.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    cout << x << endl;
+    x = 8;
+    cout << x << endl;
+    x += 2;  // same as (x = x+2);
+    cout << x << endl;
+    x -= 8;  // same as (x = x-8);
+    cout << x << endl;
+    return 0;
+}
