4daa3f096158b09bc87d3850798d5b8ed43f6fb5
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-01 01:50:13 +0600

    Create 6_increment_decrement.cpp

---
+++ Chapter-1/6_increment_decrement.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    x++;   //post increment
+    cout << x << endl;
+    x--;   //post decrement
+    cout << x << endl;
+    ++x;   //pre increment
+    cout << x << endl;
+    --x;   //pre decrement
+    cout << x << endl;
+    return 0;
+}
