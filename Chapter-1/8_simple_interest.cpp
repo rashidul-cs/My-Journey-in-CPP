b5e5c6090646f7cc56c72a7e5e0610f49c6f3857
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-02 02:27:04 +0600

    Create 8_simple_interest.cpp

---
+++ Chapter-1/8_simple_interest.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    float p = 540;
+    float r = 32;
+    float t = 3;
+    float si = (p*r*t)/100.0;
+    cout << si << endl;
+    return 0;
+}
