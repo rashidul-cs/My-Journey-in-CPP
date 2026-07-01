8e76eb2ab66bb758ecb9b3f308557734d204d1b1
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-02 02:31:43 +0600

    Create 10_sum_user_input.cpp

---
+++ Chapter-1/10_sum_user_input.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x, y;
+    cout << "Enter 1st number: ";
+    cin >> x;
+    cout << "Enter 2nd number: ";
+    cin >> y;
+    cout << "Sum: " << x+y;
+    return 0;
+}
