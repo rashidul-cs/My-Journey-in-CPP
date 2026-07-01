d8589e52065c9f141287d4242cac0d5ff945b09e
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-02 02:29:48 +0600

    Create 9_square_a_number_user_input.cpp

---
+++ Chapter-1/9_square_a_number_user_input.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Square of this number is: ";
+    cout << x*x;
+    return 0;
+}
