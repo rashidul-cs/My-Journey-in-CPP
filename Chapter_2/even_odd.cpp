3faf7c6052ce4a2e1f9b6fb2f752a7da64b317c2
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-06 14:07:03 +0600

    Create even_odd.cpp

---
+++ Chapter_2/even_odd.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    if (n%2 == 0){
+        cout << n << " is a even number";
+    } else {
+        cout << n << " is a odd number";
+    }
+    return 0;
+}
