4c21fa429fc342af8ff163fbfb68558def0eb1bc
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-07 17:43:42 +0600

    Create 10_ternary.cpp

---
+++ Chapter_2/10_ternary.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter marks: ";
+    cin >> n;
+    (n%2 == 0)? cout << "Even number" : cout << "Odd number";
+    return 0;
+}
