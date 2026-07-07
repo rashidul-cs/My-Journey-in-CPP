c07974d21555458e658f4441ed54b40c52dd58f5
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-07 17:45:39 +0600

    Create 13_practice.cpp

---
+++ Chapter_2/13_practice.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 3, y, z;
+    y = x = 10;
+    z = x < 10;   // Bolean True False working here
+    cout << x << " " << y << " " << z;
+    return 0;
+}
