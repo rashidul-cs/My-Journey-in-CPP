2dbf07e96a34d48e546679f505bdd4512e8b67ed
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-11 16:07:53 +0600

    Create 7_geometric_series_noformula.cpp

---
+++ Chapter-3/7_geometric_series_noformula.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    //1, 2, 4, 8, 16...
+    int a = 1;
+    for (int i = 1; i<=n; i++){
+        cout << a << " ";
+        a *= 2;
+    }
+    return 0;
+}
