b59d9af3ab1c04cbe00f6335e716972099ae0704
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-11 16:05:20 +0600

    Create 6_arithmetic_series.cpp

---
+++ Chapter-3/6_arithmetic_series.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    //1, 3, 5, 7, 9...
+    //formula = (a*(n-1)*d)
+    for (int i = 1; i<=(2*n)-1; i+=2){
+        cout << i << " ";
+    }
+    return 0;
+}
