dab245d9e3744fc22ec80da998bc36107184a387
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-08 13:26:59 +0600

    Create 1-star rectangle.cpp

---
+++ Chapter-4/1-star rectangle.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    //rectangle have to do
+    //rows = m, col = 5
+    int m;
+    cout << "Enter the number of rows: ";
+    cin >> m;
+    for (int i = 1; i<=m; i++){
+        cout << "* * * *" << endl;
+    }
+    return 0;
+}
