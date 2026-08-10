a8e06ba70b8f0993dd13290da5b91515bea8d027
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-11 01:31:14 +0600

    Create 2_arithmetic-operators.cpp

---
+++ hw/2_arithmetic-operators.cpp
@@ -0,0 +1,10 @@
+
+/*1 we should use here 7.0 or 22.0 or 3.0 or 5.0,
+if we want real output*/
+#include <iostream>
+using namespace std;
+int main(){
+    float a = 7/22*(3.14+2)*3/5;
+    cout << a << endl;
+    return 0;
+}
