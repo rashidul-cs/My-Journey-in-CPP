acd38abb4924bbc1b8b25b13ba5d2b0d5a59a341
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-16 21:25:03 +0600

    Create 4_return-type-sum.cpp

---
+++ Chapter-5/4_return-type-sum.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int sum (int a, int b){
+    return a+b;
+}
+int main(){
+    cout << sum(72, 9);
+    return 0;
+}
+
+//5. library functions
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout << sqrt(3.6); // sqrt = square root
+    return 0;
+}
