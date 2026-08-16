538b56da540e41963bdd00899fd0736de8d21955
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-16 21:22:40 +0600

    Create 1_basic-functions.cpp

---
+++ Chapter-5/1_basic-functions.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+void usa(){
+        cout << "You are in USA" << endl;
+    }
+void bangladesh(){
+    cout << "You are in Bangladesh" << endl;
+}
+int main(){
+    cout << "You are in main function" << endl;
+    usa();
+    bangladesh();
+    return 0;
+}
