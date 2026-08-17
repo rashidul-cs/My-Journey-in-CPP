dfa26d806a13658aac0b7e390740adada779077d
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-18 01:35:28 +0600

    Create 9_swap-without-using-extra-variable.cpp

---
+++ Chapter-5/9_swap-without-using-extra-variable.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 12;
+    int y = 45;
+    cout << x << " " << y << endl;
+    x = x+y;
+    y = x-y;
+    x = x-y;
+    cout << x << " " << y << endl;
+    return 0;
+}
