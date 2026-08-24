e56eb15e9c454e68a89e953cf67517858c61d952
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-24 21:09:33 +0600

    Create 5.3_library-function.cpp

---
+++ Chapter-5/5.3_library-function.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout << max(4, 8) << endl; // find maximum
+    cout << min(4, 8) << endl; // find minimum
+    cout << pow(2, 10) << endl; // find to the power (2^10)
+    return 0;
+}
