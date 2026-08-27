094b778e7c3543569c39f2d316d558271f00b6f5
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-27 21:57:33 +0600

    Create 2_array-initialization.cpp

---
+++ Chapter-8/2_array-initialization.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5] = {4, 1, 3, 7, 3};
+    // int arr[] = {4, 1, 3, 7, 3}; //we can also initialize array like this
+    for (int i = 0; i<=4; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
