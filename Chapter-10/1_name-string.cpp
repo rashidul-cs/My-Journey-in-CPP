620d3ea0e2bfb5c36b777008b3108d09bdfac2bc
Md Rashidul Islam <rashid6788g@gmail.com>
2026-09-03 19:59:02 +0600

    Create 1_name-string.cpp

---
+++ Chapter-10/1_name-string.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    char arr[8] = {'R', 'a', 's', 'h', 'i', 'd', 'u', 'l'};
+    for (int i = 0; i<8; i++){
+        cout << arr[i];
+    }
+    return 0;
+}
