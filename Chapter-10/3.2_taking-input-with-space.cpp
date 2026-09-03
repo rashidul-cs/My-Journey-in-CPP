07b34ed4ff2946b4697386b9147962b93e7fcd7b
Md Rashidul Islam <rashid6788g@gmail.com>
2026-09-03 20:01:20 +0600

    Create 3.2_taking-input-with-space.cpp

---
+++ Chapter-10/3.2_taking-input-with-space.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s;
+    cout << "Enter a string: ";
+    getline(cin, s);    //Input with space
+    cout << "String is: " << s << endl;
+    return 0;
+}
