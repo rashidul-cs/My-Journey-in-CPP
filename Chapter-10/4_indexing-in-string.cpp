7bf867a22d8adff55e08b36bdc01c48d5bca5b5e
Md Rashidul Islam <rashid6788g@gmail.com>
2026-09-04 20:49:11 +0600

    Create 4_indexing-in-string.cpp

---
+++ Chapter-10/4_indexing-in-string.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s = "Raghav Garg";
+    cout << s << endl;
+    s[0] = 'M';
+    s[2] = 'd';
+    cout << s << endl;
+    return 0;
+}
