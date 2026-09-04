d160bcd3d72103f1c426ee2b6257f96a95721627
Md Rashidul Islam <rashid6788g@gmail.com>
2026-09-04 20:50:18 +0600

    Create 5_length-of-string.cpp

---
+++ Chapter-10/5_length-of-string.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s = "Raghav ";  //in string there is always a Null Character in the last[ '\0' ]
+    cout << s.length(); // we can also use here [ s.size() ]
+    //cout << endl;
+    //char ch = '\0';
+    //cout << (int)ch;
+    //cout << endl;
+    //int x = 0;
+    //cout << (char) x;  //that means Null Character (ASCCI Value)
+    return 0;
+}
