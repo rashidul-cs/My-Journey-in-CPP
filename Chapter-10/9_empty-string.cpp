2a9786adac387b6c78fd5abbd2c7f247c90d7bba
Md Rashidul Islam <rashid6788g@gmail.com>
2026-09-05 21:40:11 +0600

    Create 9_empty-string.cpp

---
+++ Chapter-10/9_empty-string.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    string s = "Raghav";
+    cout << s << " " << s.length() << endl;
+    s.clear();     //for empty the string
+    cout << s << " " << s.length() << endl;
+    return 0;
+}
